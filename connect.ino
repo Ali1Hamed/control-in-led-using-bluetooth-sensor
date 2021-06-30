adff8b722e0636fbb5a43a3ffac142eb46a881f1
Ali1Hamed <63613577+Ali1Hamed@users.noreply.github.com>
2021-07-01 01:20:59 +0200

    Add files via upload

---
+++ connect.ino
@@ -0,0 +1,24 @@
+//  this program to control in led using bluetooth sensor 
+//  
+
+int led =6; // define the led pin 
+char val;   // variable to read data in it
+void setup() {
+  Serial.begin(9600);     // start serial 
+  pinMode(led,OUTPUT);    // made led pin as output pin 
+
+}
+
+void loop() {
+  val=Serial.read();    // read 1 or 2 from the sensor 
+  // if the value is 1 make led on
+  if(val=='1')          
+  {
+    digitalWrite(led,HIGH);
+  }
+  // if the value is 1 make led off
+  else if(val=='2')
+  {
+    digitalWrite(led,LOW);
+  }
+}
